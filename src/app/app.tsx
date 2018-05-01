21e50432687881e5012ab861c3be2e4f95bdb009
Andrey <aa@stepcart.com>
2018-05-02 00:26:12 +0300

    bootstrap style integration

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import {Button} from "./components/button/button";
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 export class App extends React.Component {
 
     public render(){
         return (
-            <div>
+            <div className="container">
                 <Button title="Hell"/>
             </div>
         )
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
         rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
 
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
@@ -32,6 +33,7 @@ module.exports = {
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
     externals: {
+        "socket.io-client": "io"
         // "react": "React",
         // "react-dom": "ReactDOM"
     },
